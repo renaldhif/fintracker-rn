58fafd8b8f506fcf795588b6a865f2030a635bd7
Renaldhi Fahrezi <renaldhifahrezi48@gmail.com>
2024-03-16 22:35:35 +0800

    init project and add CustomText

---
+++ src/CustomText.tsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { StyleSheet, Text, TextStyle, StyleProp } from 'react-native';
+
+export type CustomTextProps = {
+  style?: StyleProp<TextStyle>;
+  fontWeight?: 'Thin' | 'Light' | 'Regular' | 'Medium' | 'Bold' | 'Heavy';
+  fontVariant?: 'XS' | 'S' | 'M' | 'L' | 'XL' | 'XXL';
+  children: React.ReactNode;
+};
+
+const defaultStyle = StyleSheet.create({
+  text: {
+    fontFamily: 'AvenirNextCyr-Regular',
+    fontSize: 16,
+    color: 'black',
+  },
+});
+
+export const CustomText = ({ style, children, fontWeight, fontVariant }: CustomTextProps) => {
+  let dynamicStyle: TextStyle = {};
+
+  switch (fontWeight) {
+    case 'Thin':
+      dynamicStyle = { ...dynamicStyle, fontFamily: 'AvenirNextCyr-Thin' };
+      break;
+    case 'Light':
+      dynamicStyle = { ...dynamicStyle, fontFamily: 'AvenirNextCyr-Light' };
+      break;
+    case 'Regular':
+      dynamicStyle = { ...dynamicStyle, fontFamily: 'AvenirNextCyr-Regular' };
+      break;
+    case 'Medium':
+      dynamicStyle = { ...dynamicStyle, fontFamily: 'AvenirNextCyr-Medium' };
+      break;
+    case 'Bold':
+      dynamicStyle = { ...dynamicStyle, fontFamily: 'AvenirNextCyr-Bold' };
+      break;
+    case 'Heavy':
+      dynamicStyle = { ...dynamicStyle, fontFamily: 'AvenirNextCyr-Heavy' };
+      break;
+  }
+
+  switch (fontVariant) {
+    case 'XS':
+      dynamicStyle = { ...dynamicStyle, fontSize: 12 };
+      break;
+    case 'S':
+      dynamicStyle = { ...dynamicStyle, fontSize: 14 };
+      break;
+    case 'M':
+      dynamicStyle = { ...dynamicStyle, fontSize: 16 };
+      break;
+    case 'L':
+      dynamicStyle = { ...dynamicStyle, fontSize: 18 };
+      break;
+    case 'XL':
+      dynamicStyle = { ...dynamicStyle, fontSize: 20 };
+      break;
+    case 'XXL':
+      dynamicStyle = { ...dynamicStyle, fontSize: 24 };
+      break;
+  }
+
+  return (
+    <Text style={[defaultStyle.text, style, dynamicStyle]}>{children}</Text>
+  );
+};
